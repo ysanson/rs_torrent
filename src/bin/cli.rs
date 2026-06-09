8f51420403a9afaeda2a8fbf2157c64d1f1d6022
Yvan SANSON <sansonyvan@gmail.com>
2026-06-09 19:40:20 +0900

    Updated params ordering
    Signed-off-by: Yvan SANSON <sansonyvan@gmail.com>

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -7,18 +7,20 @@ use rs_torrent::{download_from_magnet, download_from_torrent_file};
 struct Args {
     #[command(subcommand)]
     command: Commands,
-    #[arg(short, long)]
-    output_file: String,
 }
 #[derive(Subcommand, Debug)]
 enum Commands {
     File {
-        #[arg(short, long)]
+        /// The torrent file to download
         file: String,
+        #[arg(short, long)]
+        output_file: String,
     },
     Magnet {
-        #[arg(short, long)]
+        /// The magnet URI to download
         magnet: String,
+        #[arg(short, long)]
+        output_file: String,
     },
 }
 
@@ -26,10 +28,10 @@ enum Commands {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     env_logger::builder().format_timestamp(None).init();
-    if let Commands::File { file } = &args.command {
-        download_from_torrent_file(file, &args.output_file).await?;
-    } else if let Commands::Magnet { magnet } = &args.command {
-        download_from_magnet(magnet, &args.output_file).await?;
+    if let Commands::File { file, output_file } = &args.command {
+        download_from_torrent_file(file, output_file).await?;
+    } else if let Commands::Magnet { magnet, output_file } = &args.command {
+        download_from_magnet(magnet, output_file).await?;
     }
     Ok(())
 }
